2c2b83718b57aaed76122af94b0df19ede70b283
Chris John <chrisjohnbelaos@gmail.com>
2022-08-08 15:35:57 +0800

    Add Printed8112

---
+++ e-FORS/App_Code/Printed8112.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Printed8112
+/// </summary>
+public class Printed8112
+{
+    public string CONTROLNO { get; set; }
+    public string SUBCONTROLNO { get; set; }
+    public string USERID { get; set; }
+    public Printed8112()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
